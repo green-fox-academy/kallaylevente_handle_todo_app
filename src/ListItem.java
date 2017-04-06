6800cdf7d27659549624a7a60363ff6356716cf5
Kallay Levente <kallaylevente@gmail.com>
2017-04-06 14:10:47 +0200

    finish the list case with empty list aswell

--- src/ListItem.java
+++ src/ListItem.java
@@ -1,17 +1,24 @@
+import java.util.ArrayList;
+
 public class ListItem {
   private String task;
   private String status;
+  static int counter = 0;
+  public int id = 0;
+
 
   public ListItem(String task, String status) {
     this.task = task;
     this.status = status;
+    this.counter  = counter + 1;
+    this.id = this.counter;
   }
 
   @Override
   public String toString() {
-    return "ListItem{" +
-      "type='" + task + '\'' +
-      ", status='" + status + '\'' +
-      '}';
+    String result = "";
+    // 1 - Walk the dog
+    result = (this.id + " - " + task );
+    return result;
   }
 }
--- src/Todo.java
+++ src/Todo.java
@@ -11,17 +11,35 @@ public class Todo {
   public static void main(String[] args) {
     List<String> rawLines;
     rawLines = readInput();
-    System.out.println(rawLines);
+    //System.out.println(rawLines);
     List<ListItem> listItems = new ArrayList<>();
     listItems = extractListItem(rawLines);
-    System.out.println(listItems);
+   // System.out.println(listItems);
 
     if (args.length == 0) {
-      
-    }
-
+      emptyCase();
+    } else if ((args[0].equals("-l" )&& (args.length == 1))) {
+      if (listItems.size() == 0) {
+        System.out.println("No todos for today! :)");
+      } else {
+        for (int i = 0; i < listItems.size(); i++) {
+          System.out.println(listItems.get(i));
+        }
+      }
+    } else if (args[0].equals("-a") && )
 
   }
+
+    public static void emptyCase(){
+      System.out.println("Java Todo application\n" +
+        "=======================\n" +
+        "\n" +
+        "Command line arguments:\n" +
+        " -l   Lists all the tasks\n" +
+        " -a   Adds a new task\n" +
+        " -r   Removes an task\n" +
+        " -c   Completes an task");
+    }
 
   public static List<ListItem> extractListItem(List<String> rawLines){
     List<ListItem> listItems = new ArrayList<>();
